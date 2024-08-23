e381f86963cbb2d66d3079a927bb3c91674dd3b1
ayomahansani <ayohansi17@gmail.com>
2024-08-23 23:38:31 +0530

    write code of ItemDAOImpl class

--- src/main/java/lk/ijse/thogakade_pos_backend/dao/ItemDAOImpl.java
+++ src/main/java/lk/ijse/thogakade_pos_backend/dao/ItemDAOImpl.java
@@ -1,35 +1,78 @@
 package lk.ijse.thogakade_pos_backend.dao;
 
+import lk.ijse.thogakade_pos_backend.entity.Customer;
 import lk.ijse.thogakade_pos_backend.entity.Item;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public final class ItemDAOImpl implements ItemDAO {
 
-    static String SAVE_CUSTOMER = "INSERT INTO Customer (id,name,address,phone) VALUES (?,?,?,?)";
-    static String UPDATE_CUSTOMER = "UPDATE Customer SET name=?,address=?,phone=? WHERE id=?";
-    static String DELETE_CUSTOMER = "DELETE FROM Customer WHERE id=?";
-    static String GET_CUSTOMER = "SELECT * FROM Customer";
+    static String SAVE_ITEM = "INSERT INTO Item (code,name,price,qty) VALUES (?,?,?,?)";
+    static String UPDATE_ITEM = "UPDATE Item SET name=?,price=?,qty=? WHERE code=?";
+    static String DELETE_ITEM = "DELETE FROM Item WHERE code=?";
+    static String GET_ITEM = "SELECT * FROM Item";
 
     @Override
     public boolean save(Item item, Connection connection) throws SQLException {
-        return false;
+
+        var ps = connection.prepareStatement(SAVE_ITEM);
+
+        ps.setString(1, item.getCode());
+        ps.setString(2, item.getName());
+        ps.setString(3, String.valueOf(item.getPrice()));
+        ps.setString(4, String.valueOf(item.getQty()));
+
+        return ps.executeUpdate() != 0;
     }
 
     @Override
     public boolean update(String itemCode, Item item, Connection connection) throws SQLException {
-        return false;
+
+        var ps = connection.prepareStatement(UPDATE_ITEM);
+
+        ps.setString(1, item.getName());
+        ps.setString(2, String.valueOf(item.getPrice()));
+        ps.setString(3, String.valueOf(item.getQty()));
+        ps.setString(4, itemCode);
+
+        return ps.executeUpdate() != 0;
     }
 
     @Override
     public boolean delete(String itemCode, Connection connection) throws SQLException {
-        return false;
+
+        var ps = connection.prepareStatement(DELETE_ITEM);
+
+        ps.setString(1, itemCode);
+
+        return ps.executeUpdate() != 0;
     }
 
     @Override
     public List<Item> get(Connection connection) throws SQLException {
-        return List.of();
+
+        List<Item> items = new ArrayList<>();
+
+        var ps = connection.prepareStatement(GET_ITEM);
+
+        ResultSet rs = ps.executeQuery();
+
+        while (rs.next()) {
+
+            Item item = new Item();
+
+            item.setCode(rs.getString("code"));
+            item.setName(rs.getString("name"));
+            item.setPrice(Double.parseDouble(rs.getString("price")));
+            item.setQty(Integer.parseInt(rs.getString("qty")));
+
+            items.add(item);
+        }
+
+        return items;
     }
 }
