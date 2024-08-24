63c9e26aed1f6d416bc4187cbb8fc79dffd38c29
ayomahansani <ayohansi17@gmail.com>
2024-08-25 01:40:12 +0530

    created DAOFactory class

--- src/main/java/lk/ijse/thogakade_pos_backend/dao/DAOFactory.java
+++ src/main/java/lk/ijse/thogakade_pos_backend/dao/DAOFactory.java
@@ -1,4 +1,32 @@
 package lk.ijse.thogakade_pos_backend.dao;
 
+import lk.ijse.thogakade_pos_backend.dao.custom.impl.CustomerDAOImpl;
+import lk.ijse.thogakade_pos_backend.dao.custom.impl.ItemDAOImpl;
+
 public class DAOFactory {
+
+    private static DAOFactory daoFactory;
+
+    private DAOFactory(){
+    }
+
+    public static DAOFactory getDaoFactory(){
+        return (daoFactory == null) ? new DAOFactory() : daoFactory;
+    }
+
+    public enum DAOTypes{
+        CUSTOMER,ITEM
+    }
+
+    public CrudDAO getDAO(DAOTypes daoTypes){
+
+        switch (daoTypes){
+            case CUSTOMER:
+                return new CustomerDAOImpl();
+            case ITEM:
+                return new ItemDAOImpl();
+            default:
+                return null;
+        }
+    }
 }
